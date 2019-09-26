b2d76802cffd850332120a1c5303e64a925ae4f6
Raymond Zheng <raymond1wen1zheng1@gmail.com>
2019-09-26 18:33:56 -0400

    lol remove commit.sh

--- submit.sh
+++
@@ -1,9 +0,0 @@
-cd /Users/raymond/c1/C1GamesStarterKit
-rm v3.zip
-mkdir compile_134789
-cp -r $1 compile_134789/python-algo
-cd compile_134789
-zip -r ../v3.zip python-algo -x "*__pycache__*" -x "*errorFile*"
-cd ..
-rm -r compile_134789
-
